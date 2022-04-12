5669969a91d356bda03cf18a64eb5a0d1f8990d9
androidtempley <103524996+androidtempley@users.noreply.github.com>
2022-04-12 20:14:37 +0100

    Create basic example application

--- app/src/main/java/com/github/androidtempley/sectorpicker/MainActivity.kt
+++ app/src/main/java/com/github/androidtempley/sectorpicker/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.github.androidtempley.sectorpicker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var sectorPicker: SectorPicker
+    private lateinit var seekBar: SeekBar
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sectorPicker = findViewById(R.id.sectorPicker)
+        seekBar = findViewById(R.id.seekBar)
+        textView = findViewById(R.id.statsText)
+
+        seekBar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onStopTrackingTouch(seekBar: SeekBar) {  }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) { }
+
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                sectorPicker.numberOfPoints = progress
+            }
+        })
+
+        Thread {
+            while(true) {
+                updateStatsText()
+
+                Thread.sleep(500)
+            }
+        }.start()
+    }
+
+    private fun updateStatsText() {
+        val marker1Pos = sectorPicker.getMarkerPosition(SectorPicker.MARKER_1)
+        val marker2Pos = sectorPicker.getMarkerPosition(SectorPicker.MARKER_2)
+
+        textView.text = String.format(getString(R.string.status), marker1Pos, marker2Pos)
     }
 }
