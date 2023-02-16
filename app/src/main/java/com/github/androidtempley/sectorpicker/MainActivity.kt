fb5400893b69f2adeb14e66a687cb592e2a601e3
Henry Smith <henrytsmith95@gmail.com>
2023-02-16 12:45:51 +0000

    Added code back in from picker updates

--- app/src/main/java/com/github/androidtempley/sectorpicker/MainActivity.kt
+++ app/src/main/java/com/github/androidtempley/sectorpicker/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.github.androidtempley.sectorpickerexample
 
+import android.content.Context
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Log
 import android.widget.SeekBar
 import android.widget.SeekBar.OnSeekBarChangeListener
 import android.widget.TextView
@@ -8,12 +11,23 @@ import androidx.appcompat.app.AppCompatActivity
 import com.github.androidtempley.sectorpicker.SectorPicker
 import com.github.androidtempley.sectorpicker.SectorPickerEventListener
 
+import android.graphics.drawable.Drawable
+import androidx.core.content.res.ResourcesCompat
+import android.graphics.Bitmap
+import android.graphics.Canvas
+
+import android.graphics.drawable.BitmapDrawable
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var sectorPicker: SectorPicker
     private lateinit var seekBar: SeekBar
     private lateinit var textView: TextView
+    private var centreButtonState = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,6 +53,26 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
+        val bitmapFile =
+            ResourcesCompat.getDrawable(resources, R.drawable.ic_android_blue_24dp, null)?.let {
+                drawableToBitmap(
+                    it
+                )
+            }
+
+        if (bitmapFile == null) {
+            Log.e("MainActivity", "Bitmap is null...")
+        }
+        else {
+            sectorPicker.setButtonBitmap(bitmapFile)
+        }
+
+        sectorPicker.setOnButtonClickListener {
+            sectorPicker.setFullCircleMode(!centreButtonState)
+            sectorPicker.setMarkerState(centreButtonState)
+            centreButtonState = !centreButtonState
+        }
+
         updateStatsText()
     }
 
@@ -47,5 +81,17 @@ class MainActivity : AppCompatActivity() {
         val marker2Pos = sectorPicker.getMarkerPosition(SectorPicker.MARKER_2)
 
         textView.text = String.format(getString(R.string.status), marker1Pos, marker2Pos)
+    }
+
+    fun drawableToBitmap(drawable: Drawable): Bitmap? {
+        if (drawable is BitmapDrawable) {
+            return drawable.bitmap
+        }
+        val bitmap =
+            Bitmap.createBitmap(drawable.intrinsicWidth, drawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight())
+        drawable.draw(canvas)
+        return bitmap
     }
 }
